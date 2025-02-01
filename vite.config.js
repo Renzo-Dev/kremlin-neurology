514e8c331c547620039642fa956b8f38fef5913f
Dan Koshevoy <dankoshevoy2001@gmail.com>
2025-02-01 19:16:08 +0300

    up

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import Components from 'unplugin-vue-components/vite'
-import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [BootstrapVueNextResolver()],
-    }),
-  ],
+  plugins: [vue()],
   server: {
     port: 3000, // Укажите нужный порт, если требуется
     open: false, // Открывать ли браузер по адресу
