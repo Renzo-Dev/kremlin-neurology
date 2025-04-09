22d34ed043015ae64163559043c1ab41174c4b7a
Administrator <dankoshevoy2001@gmail.com>
2025-04-09 21:17:52 +0300

    Обновлены обработчики событий для выпадающего меню в компоненте navPanel.vue: исправлены ошибки в логике отображения и скрытия меню, улучшена реактивность свойств. Также внесены изменения в стили для соответствия новым требованиям.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import { router } from '@/routes/route'
 import { createApp } from 'vue'
 import App from './App.vue'
-// main.js/ts
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
