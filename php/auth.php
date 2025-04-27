057bd3989b80a403822abc198d7ff35baa8d4746
Renzo-Dev <dankoshevoy2001@gmail.com>
2025-04-27 22:44:26 +0300

    fix

--- php/auth.php
+++ php/auth.php
@@ -1,13 +1,36 @@
 <?php
 session_start();
 
-// пароль от библиотеки
-define('ACCESS_PASSWORD', 'mypass');
+header('Content-Type: application/json');
 
-if (isset($_POST['password']) && $_POST['password'] == ACCESS_PASSWORD) {
-    $_SESSION['authenticated'] = true;
-    $_SESSION['expires_at'] = time() + 3600; // 1 h
-    echo 'success';
+// Задаем пароль
+define('ACCESS_PASSWORD', '123123');
+
+// Проверяем отправленный пароль
+if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+
+    if ($password == ACCESS_PASSWORD) {
+        $_SESSION['authenticated'] = true;
+        $_SESSION['expires_at'] = time() + 3600; // 1h
+        echo json_encode([
+            'success' => true,
+            'message' => 'Logged in successfully.',
+        ]);
+        exit();
+    } else {
+        http_response_code(401);
+        echo json_encode([
+            'success' => false,
+            'message' => 'Wrong password.',
+        ]);
+        exit();
+    }
 } else {
-    echo 'error';
+    http_response_code(401);
+    echo json_encode([
+        'success' => false,
+        'message' => 'Password not provided.',
+    ]);
+    exit();
 }
--- php/check_session.php
+++ php/check_session.php
@@ -1,11 +1,26 @@
 <?php
-session_start();
 
-if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true || time() > $_SESSION['expires_at']) {
-    // Нет доступа — отправляем статус 403
-    header('HTTP/1.1 403 Forbidden');
-    echo 'Доступ запрещён';
+header('Content-type: application/json');
+
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    http_response_code(402);
+    echo json_encode([
+        'authenticated' => false,
+        'error' => 'Access denied: You are not authenticated.'
+    ]);
     exit();
 }
 
-echo 'Доступ разрешён';
+if (time() > $_SESSION['expires_at']) {
+    session_destroy();
+    http_response_code(403);
+    echo json_encode([
+        'authenticated' => false,
+        'error' => 'Access denied: Session expired.'
+    ]);
+}
+
+echo json_encode([
+    'authenticated' => true,
+    'message' => 'Access granted.'
+]);
