92a66c1dd0dd0699eba8519237586044ef1854d9
Renzo-Dev <dankoshevoy2001@gmail.com>
2025-07-20 02:02:47 +0300

    Refactor Header components and styles: extract scoped styles into dedicated SCSS files, update CSS variables for consistency, enhance NavList styling, and add new navigation items.

--- frontend/src/components/Header/TextHeader.vue
+++ frontend/src/components/Header/TextHeader.vue
@@ -13,43 +13,5 @@
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-.header-text {
-  width: 100%;
-  height: auto;
-  display: flex;
-  align-items: center;
-
-  a {
-    width: auto;
-    height: auto;
-    text-wrap: balance;
-    padding: 1rem;
-    text-decoration: none;
-    color: var(--text-color);
-    font-size: 1.4rem;
-    font-weight: 500;
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    justify-content: center;
-    transition: 0.2s ease;
-
-    &:hover {
-      color: var(--hv-text-color);
-    }
-
-    div {
-      height: 100%;
-      font-size: 1.4rem;
-      font-weight: 400;
-    }
-
-    h5 {
-      margin-top: 0.5rem;
-      height: 100%;
-      font-size: 1.5rem;
-      font-weight: 500;
-    }
-  }
-}
+@use '@/assets/styles/Header/TextHeader.scss';
 </style>
