17b44c48a59de349969bf8886c4681289356bb04
Renzo-Dev <dankoshevoy2001@gmail.com>
2025-07-21 20:37:49 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   server: {
     port: 3000, // Укажите нужный порт, если требуется
     open: false, // Открывать ли браузер по адресу
